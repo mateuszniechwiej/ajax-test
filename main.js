530cfaa22d8fdb5b363036c5ac0e282e7a2ef82d
Mateusz Niechwiej <niechwiej.mateusz@gmail.com>
2021-01-26 09:50:18 +0000

    callbacks

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-var xhr = new XMLHttpRequest();
-var data;
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-xhr.send();
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.send();
 
-function setData(jsonData) {
-    data = jsonData;
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+function printDataToConsole(data) {
     console.log(data);
 }
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        setData(JSON.parse(this.responseText));        
-    }
-};
-
-console.log('data');
+getData(printDataToConsole);
