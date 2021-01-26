73d227ecb974e9b6e75ee89b416ba0095918c46c
Mateusz Niechwiej <niechwiej.mateusz@gmail.com>
2021-01-26 13:09:23 +0000

    tabular data p2

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const baseURL = "https://ci-swapi.herokuapp.com/api/";
 function getData(type, cb) {
     var xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
         }
@@ -16,7 +16,7 @@ function getData(type, cb) {
 function getTableHeaders(obj) {
     var tableHeaders = [];
 
-    Object.keys(obj).forEach(function (key) {
+    Object.keys(obj).forEach(function(key) {
         tableHeaders.push(`<td>${key}</td>`)
     });
 
@@ -24,17 +24,25 @@ function getTableHeaders(obj) {
 }
 
 function writeToDocument(type) {
+    var tableRows = [];
     var el = document.getElementById("data");
     el.innerHTML = "";
 
-    getData(type, function (data) {
+    getData(type, function(data) {
+        
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
 
-        data.forEach(function (item) {
-            // el.innerHTML += "<p>" + item.name + "</p>";
+        data.forEach(function(item) {
+            var dataRow = [];
+
+            Object.keys(item).forEach(function(key) {
+                dataRow.push(`<td>${item[key]}</td>`);
+            });
+            tableRows.push(dataRow)
         });
 
-        el.innerHTML = `<table>${tableHeaders}</table>`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
     });
 }
+
